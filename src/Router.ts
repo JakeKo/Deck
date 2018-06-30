c512f680044c961b127c690f4f19b3b56331dcce
Jacob Koperski <wijakoperski@gmail.com>
2018-06-30 13:17:09 -0500

    Renamed App.vue to Index.vue

--- src/Router.ts
+++ src/Router.ts
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-import App from "./routes/App.vue";
+import Index from "./routes/Index.vue";
 
 Vue.use(VueRouter);
 
@@ -9,11 +9,11 @@ export default new VueRouter({
 	routes: [
 		{
 			path: "/foo",
-			component: App
+			component: Index
 		},
 		{
 			path: "/bar",
-			component: App
+			component: Index
 		}
 	]
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import App from "./routes/App.vue";
+import Index from "./routes/Index.vue";
 import Store from "./Store";
 import Router from "./Router";
 
@@ -7,5 +7,5 @@ new Vue({
 	el: "#app",
 	store: Store,
 	router: Router,
-	render: h => h(App)
+	render: h => h(Index)
 });
