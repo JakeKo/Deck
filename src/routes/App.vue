e74440906eff145ca61a4943bd77f76c9d5c9030
Jacob Koperski <wijakoperski@gmail.com>
2018-07-02 22:52:59 -0500

    Mocked out main app page

---
+++ src/routes/App.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<toolbox></toolbox>
+
+		<widgets></widgets>
+
+		<workspace>
+			<canvas>
+				<slide></slide>
+			</canvas>
+
+			<roadmap>
+				<slide-preview></slide-preview>
+				<slide-preview></slide-preview>
+				<slide-preview></slide-preview>
+				<slide-preview></slide-preview>
+				<slide-preview></slide-preview>
+			</roadmap>
+		</workspace>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+	
+});
+</script>
