c01948400286988b80b3590279f3b6a5e786c633
Jacob Koperski <wijakoperski@gmail.com>
2018-07-07 08:22:50 -0500

    Added a template file for formatting vue files

---
+++ src/__TEMPLATE__.vue
@@ -0,0 +1,20 @@
+/* tslint:disable */
+<template>
+    
+</template>
+
+<script lang="ts">
+/* tslint:enable */
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class MyComponent extends Vue {
+    
+}
+/* tslint:disable */
+</script>
+
+<style lang="sass" scoped>
+
+</style>
