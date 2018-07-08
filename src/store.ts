1fabf087dc5580614198cf06e8093966489bc52a
Jacob Koperski <wijakoperski@gmail.com>
2018-07-08 02:34:36 -0500

    Added Vuex mutations to add a slide or shape

--- src/store.ts
+++ src/store.ts
@@ -3,17 +3,27 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+const generateId = () => {
+    const s4 = () => Math.floor(( Math.random() + 1) * 0x10000).toString(16).substring(1).toLocaleUpperCase();
+    return `${s4()}${s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
+};
+
 export default new Vuex.Store({
     state: {
         slides: [
             {
+                id: generateId(),
+                previous: "",
+                next: "",
                 shapes: [
                     {
+                        id: generateId(),
                         backgroundColor: "pink",
                         height: "200px",
                         width: "200px"
                     },
                     {
+                        id: generateId(),
                         backgroundColor: "blue",
                         height: "150px",
                         width: "200px"
@@ -21,5 +31,25 @@ export default new Vuex.Store({
                 ]
             }
         ]
+    },
+    mutations: {
+        addSlide(state) {
+            state.slides.push({
+                id: generateId(),
+                previous: state.slides[state.slides.length - 1].id,
+                next: "",
+                shapes: []
+            });
+        },
+        addShape(state, { slideId, shape }) {
+            const slide = state.slides.filter((s) => s.id === slideId)[0];
+            const newShape = shape || {};
+
+            if (newShape.id === undefined) {
+                newShape.id = generateId();
+            }
+
+            slide.shapes.push(newShape);
+        }
     }
 });
