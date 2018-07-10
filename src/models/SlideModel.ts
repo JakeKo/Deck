ee17881cdf8eebae65b93ac62ef813a494f3bfb8
Jacob Koperski <wijakoperski@gmail.com>
2018-07-09 21:41:46 -0500

    Added SlideModel for... you know... modeling slides

---
+++ src/models/SlideModel.ts
@@ -0,0 +1,32 @@
+import { GenerateId } from "../utilities/models";
+import ShapeModel from "./ShapeModel";
+
+export default class SlideModel {
+    private _id: String;
+    public get id(): String { return this._id; }
+    public set id(value: String) { this._id = value; }
+
+    private _active: Boolean;
+    public get active(): Boolean { return this._active; }
+    public set active(value: Boolean) { this._active = value; }
+
+    private _previous?: String;
+    public get previous(): String | undefined { return this._previous; }
+    public set previous(value: String | undefined) { this._previous = value; }
+
+    private _next?: String;
+    public get next(): String | undefined { return this._next; }
+    public set next(value: String | undefined) { this._next = value; }
+
+    private _shapes: Array<ShapeModel>;
+    public get shapes(): Array<ShapeModel> { return this._shapes; }
+    public set shapes(value: Array<ShapeModel>) { this._shapes = value; }
+
+    constructor(id?: String, active?: Boolean, previous?: String, next?: String, shapes?: Array<ShapeModel>) {
+        this._id = id || GenerateId();
+        this._active = active || false;
+        this._previous = previous;
+        this._next = next;
+        this._shapes = shapes || [];
+    }
+}
--- src/store.ts
+++ src/store.ts
@@ -1,28 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import Shape from "./components/Shape.vue";
 import ShapeModel from "./models/ShapeModel";
+import SlideModel from "./models/SlideModel";
 
 Vue.use(Vuex);
-
-const generateId = () => {
-    const s4 = () => Math.floor((Math.random() + 1) * 0x10000).toString(16).substring(1).toLocaleUpperCase();
-    return `${s4()}${s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
-};
 
 export default new Vuex.Store({
     state: {
         slides: [
-            {
-                id: generateId(),
-                active: true,
-                previous: "",
-                next: "",
-                shapes: [
-                    new ShapeModel(),
-                    new ShapeModel(),
-                ]
-            }
+            new SlideModel(undefined, true)
         ]
     },
     getters: {
@@ -38,13 +24,9 @@ export default new Vuex.Store({
     },
     mutations: {
         addSlide: (state) => {
-            state.slides.push({
-                id: generateId(),
-                active: false,
-                previous: "",
-                next: "",
-                shapes: []
-            });
+            const lastSlideId = state.slides.length > 0 ? state.slides[state.slides.length - 1].id : undefined;
+
+            state.slides.push(new SlideModel(undefined, false, lastSlideId));
         },
         addShapeToSlide: (state, { slideId, shapeModel }: { slideId: String, shapeModel: ShapeModel }) => {
             const slides = state.slides.filter((s) => s.id === slideId);
