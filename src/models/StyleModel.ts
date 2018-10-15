2a8a527c43dcf58fbc5c71fd730c43fe2cb04f4c
Jacob Koperski <wijakoperski@gmail.com>
2018-10-14 23:52:23 -0500

    Created explicit index signature for TextboxStyleModel and StyleModel

--- src/models/StyleModel.ts
+++ src/models/StyleModel.ts
@@ -1,27 +1,15 @@
 export default class StyleModel {
-    private _fill: string;
-    public get fill(): string { return this._fill; }
-    public set fill(value: string) { this._fill = value; }
+    [key:string]: string;
+    public fill: string;
+    public stroke: string;
+    public strokeWidth: string;
 
-    private _stroke: string;
-    public get stroke(): string { return this._stroke; }
-    public set stroke(value: string) { this._stroke = value; }
-
-    private _strokeWidth: string;
-    public get strokeWidth(): string { return this._strokeWidth; }
-    public set strokeWidth(value: string) { this._strokeWidth = value; }
-
-    constructor({
-        fill,
-        stroke,
-        strokeWidth
-    }: {
-            fill?: string,
-            stroke?: string,
-            strokeWidth?: string
-        } = {}) {
-        this._fill = fill || "white";
-        this._stroke = stroke || "grey";
-        this._strokeWidth = strokeWidth || "1";
+    constructor(
+        { fill, stroke, strokeWidth }:
+        { fill?: string, stroke?: string, strokeWidth?: string } = {}
+    ) {
+        this.fill = fill || "white";
+        this.stroke = stroke || "grey";
+        this.strokeWidth = strokeWidth || "1";
     }
 }
--- src/models/TextboxStyleModel.ts
+++ src/models/TextboxStyleModel.ts
@@ -1,12 +1,11 @@
 export default class TextboxStyleModel {
-    private _fill: string;
-    public get fill(): string { return this._fill; }
-    public set fill(value: string) { this._fill = value; }
+    [key:string]: string;
+    public fill: string;
 
     constructor(
         { fill }:
         { fill?: string } = {}
     ) {
-        this._fill = fill || "grey";
+        this.fill = fill || "grey";
     }
 }
