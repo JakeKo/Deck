917ed92f4cc364dbe5ff1cdcfc393b15362d88ee
Jacob Koperski <wijakoperski@gmail.com>
2018-11-18 10:40:27 -0600

    Fleshed out unit tests for Tool component

--- test/Tool.spec.js
+++ test/Tool.spec.js
@@ -13,6 +13,8 @@ describe("Tool", () => {
     it("can instantiate", () => {
         const wrapper = shallowMount(Tool, { store, localVue });
         
+        expect(wrapper.exists()).toBe(true);
+        expect(wrapper.isVueInstance()).toBe(true);
         expect(wrapper.is(Tool)).toBe(true);
     });
 
@@ -20,5 +22,33 @@ describe("Tool", () => {
         const wrapper = shallowMount(Tool, { store, localVue });
         
         expect(wrapper.props("isActive")).toBe(false);
+    });
+
+    it("is highlighted when it is active", () => {
+        const wrapper = shallowMount(Tool, {
+            store,
+            localVue,
+            propsData: {
+                isActive: true
+            }
+        });
+        
+        expect(wrapper.props("isActive")).toBe(true);
+        expect(wrapper.contains("#tool-icon")).toBe(true);
+        expect(wrapper.find("#tool-icon").element.style.background).toBe("blue");
+    });
+
+    it("is muted when it is inactive", () => {
+        const wrapper = shallowMount(Tool, {
+            store,
+            localVue,
+            propsData: {
+                isActive: false
+            }
+        });
+        
+        expect(wrapper.props("isActive")).toBe(false);
+        expect(wrapper.contains("#tool-icon")).toBe(true);
+        expect(wrapper.find("#tool-icon").element.style.background).toBe("rgb(221, 221, 221)");
     });
 });
