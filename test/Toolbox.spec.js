a8d1e2140baeb988c81bcbf90b2a81ca58165617
Jacob Koperski <wijakoperski@gmail.com>
2018-11-30 21:14:49 -0600

    Fixed broken Toolbox tests

--- test/Toolbox.spec.js
+++ test/Toolbox.spec.js
@@ -2,6 +2,7 @@ import { shallowMount, createLocalVue } from "@vue/test-utils";
 import Vuex from "vuex";
 import Store from "../src/store";
 import Toolbox from "../src/components/Toolbox.vue";
+import Tool from "../src/components/Tool.vue";
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
@@ -11,30 +12,36 @@ describe("Toolbox", () => {
     beforeEach(() => store = Store);
 
     it("can instantiate", () => {
+        // Arrange
         const wrapper = shallowMount(Toolbox, { store, localVue });
         
+        // Act
+
+        // Assert
         expect(wrapper.exists()).toBe(true);
         expect(wrapper.isVueInstance()).toBe(true);
         expect(wrapper.is(Toolbox)).toBe(true);
     });
 
-    it("has four tools", () => {
+    it("has the correct number of tools", () => {
+        // Arrange
         const wrapper = shallowMount(Toolbox, { store, localVue });
-        
-        expect(wrapper.findAll(".tool").length).toBe(7);
+
+        // Act
+
+        // Assert
+        expect(wrapper.findAll(Tool).length).toBe(7);
     });
 
-    it("has one active tool", () => {
+    it("has the correct number of active and inactive tools", () => {
+        // Arrange
         const wrapper = shallowMount(Toolbox, { store, localVue });
-        
-        const icons = wrapper.findAll(".tool-icon").wrappers.map((w) => w.element);
-        expect(icons.filter((element) => element.style.background === "blue").length).toBe(1);
-    });
+        const toolWrappers = wrapper.findAll(Tool).wrappers;
 
-    it("has three inactive tools", () => {
-        const wrapper = shallowMount(Toolbox, { store, localVue });
-        
-        const icons = wrapper.findAll(".tool-icon").wrappers.map((w) => w.element);
-        expect(icons.filter((element) => element.style.background === "rgb(221, 221, 221)").length).toBe(5);
+        // Act
+
+        // Assert
+        expect(toolWrappers.filter((toolWrapper) => toolWrapper.props("isActive")).length).toBe(1);
+        expect(toolWrappers.filter((toolWrapper) => !toolWrapper.props("isActive")).length).toBe(6);
     });
 });
