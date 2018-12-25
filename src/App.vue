9871f75abd71e99ff58ede31184587bc9ef3cffe
Jake Koperski <wijakoperski@gmail.com>
2018-12-24 22:44:21 -0600

    Added copy and paste event handlers to the application

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,10 @@ import Toolbox from "./components/Toolbox.vue";
 import Editor from "./components/Editor.vue";
 import Roadmap from "./components/Roadmap.vue";
 import StyleEditor from "./components/StyleEditor.vue";
+import GraphicModel from "./models/GraphicModel";
+import SlideModel from "./models/SlideModel";
+import Utilities from "./utilities/miscellaneous";
+import Point from "./models/Point";
 
 @Component({
     components: {
@@ -37,6 +41,46 @@ export default class App extends Vue {
                 this.$store.commit("focusGraphic", undefined);
                 this.$store.commit("styleEditorObject", undefined);
             }
+        });
+
+        // Overrides the default behavior of copy to copy the graphic model of the focused graphic
+        document.addEventListener("copy", (event: Event): void => {
+            // Cast event as clipboard event and prevent from copying any user selection
+            const clipboardEvent: ClipboardEvent = event as ClipboardEvent;
+            clipboardEvent.preventDefault();
+
+            const focusedGraphicId: string | undefined = this.$store.getters.focusedGraphicId;
+            if (focusedGraphicId === undefined) {
+                return;
+            }
+
+            // Fetch the graphic model associated with the current focused graphic
+            const activeSlide: SlideModel = this.$store.getters.activeSlide;
+            const graphicModel: GraphicModel = activeSlide.graphics.find((graphicModel: GraphicModel) => graphicModel.id === focusedGraphicId)!;
+
+            // Set the clipboard data to the graphic model
+            clipboardEvent.clipboardData.setData("text/json", JSON.stringify(graphicModel));
+        });
+
+        // Override the default behavior of the paste to paste the copied graphic model
+        document.addEventListener("paste", (event: Event) => {
+            // Cast event as clipboard event
+            const clipboardEvent: ClipboardEvent = event as ClipboardEvent;
+            clipboardEvent.preventDefault();
+
+            const activeSlide: SlideModel = this.$store.getters.activeSlide;
+            const clipboardData: any = JSON.parse(clipboardEvent.clipboardData.getData("text/json"));
+
+            // Correct some loss of data and generate a new id for the new graphic model
+            clipboardData.id = Utilities.generateId();
+            if (clipboardData.styleModel.points !== undefined) {
+                clipboardData.styleModel.points = clipboardData.styleModel.points.map((point: { x: number, y: number}) => new Point(point.x, point.y));
+            }
+
+            const graphicModel: GraphicModel = new GraphicModel(clipboardData);
+            activeSlide.graphics.push(graphicModel);
+            this.$store.commit("focusGraphic", graphicModel);
+            this.$store.commit("styleEditorObject", graphicModel);
         });
     }
 
