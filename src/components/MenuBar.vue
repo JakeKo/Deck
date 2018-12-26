99e889606773ebb6665888f102450f1ad269f80c
Jake Koperski <wijakoperski@gmail.com>
2018-12-26 11:27:49 -0600

    Added menu bar component

---
+++ src/components/MenuBar.vue
@@ -0,0 +1,29 @@
+<template>
+<div id="menu-bar">
+IOU Menus
+</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class SlideSettings extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/components";
+
+#menu-bar {
+    background: $color-light;
+    border-bottom: 1px solid $color-tertiary;
+    height: 32px;
+    flex-shrink: 0;
+    color: $color-dark;
+    font-family: $font-body;
+    display: flex;
+    align-items: center;
+}
+</style>
