872dac45b5c9ede3c975a51122abec70b1b15351
Jake Koperski <wijakoperski@gmail.com>
2019-01-05 23:41:47 -0600

    Copy/paste creates a new graphic id

--- src/utilities/general.ts
+++ src/utilities/general.ts
@@ -6,7 +6,7 @@ import Curve from "../models/Curve";
 import Sketch from "../models/Sketch";
 import Text from "../models/Text";
 
-const toPrettyString: (object: any, indentDepth: number) => string = (object: any, indentDepth: number): string => {
+function toPrettyString(object: any, indentDepth: number): string {
     const properties: Array<string> = [];
     for (const property in object) {
         const value: any = object[property];
@@ -27,60 +27,53 @@ const toPrettyString: (object: any, indentDepth: number) => string = (object: an
     function space(indentDepth: number): string {
         return new Array(indentDepth * 4).fill(" ").join("");
     }
-};
+}
 
-const generateId: () => string = (): string => {
-    const s4 = () => Math.floor((Math.random() + 1) * 0x10000).toString(16).substring(1).toLocaleUpperCase();
-    return `${s4()}${s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
-};
+function generateId(): string {
+    function term(): string {
+        return Math.floor((Math.random() + 1) * 0x10000).toString(16).substring(1).toLocaleUpperCase();
+    }
+
+    return `${term()}${term()}-${term()}-${term()}-${term()}-${term()}${term()}${term()}`;
+}
 
 // Overrides the default behavior of copy to copy the graphic model of the focused graphic
-const copyHandler: (app: any) => (event: Event) => void = (app: any): (event: Event) => void => (event: Event): void => {
-    // Cast event as clipboard event and prevent from copying any user selection
-    const clipboardEvent: ClipboardEvent = event as ClipboardEvent;
-    clipboardEvent.preventDefault();
+function copyHandler(app: any): (event: Event) => void {
+    return function (event: Event): void {
+        // Cast event as clipboard event and prevent from copying any user selection
+        const clipboardEvent: ClipboardEvent = event as ClipboardEvent;
+        clipboardEvent.preventDefault();
 
-    const focusedGraphicId: string | undefined = app.$store.getters.focusedGraphicId;
-    if (focusedGraphicId === undefined) {
-        return;
-    }
+        const focusedGraphicId: string | undefined = app.$store.getters.focusedGraphicId;
+        if (focusedGraphicId === undefined) {
+            return;
+        }
 
-    // Set the clipboard data to the graphic model associated with the current focused graphic
-    const graphic: IGraphic = app.$store.getters.activeSlide.graphics.find((graphic: IGraphic) => graphic.id === focusedGraphicId)!;
-    clipboardEvent.clipboardData.setData("text/json", JSON.stringify({ model: graphic, type: typeof(graphic) }));
-};
+        // Set the clipboard data to the graphic model associated with the current focused graphic
+        const graphic: IGraphic = app.$store.getters.activeSlide.graphics.find((graphic: IGraphic) => graphic.id === focusedGraphicId)!;
+        clipboardEvent.clipboardData.setData("text/json", JSON.stringify(graphic));
+    };
+}
 
 // Override the default behavior of the paste to paste the copied graphic model
-const pasteHandler: (app: any) => (event: Event) => void = (app: any): (event: Event) => void => (event: Event): void => {
-    // Cast event as clipboard event
-    const clipboardEvent: ClipboardEvent = event as ClipboardEvent;
-    clipboardEvent.preventDefault();
+function pasteHandler(app: any): (event: Event) => void {
+    return function (event: Event): void {
+        // Cast event as clipboard event
+        const clipboardEvent: ClipboardEvent = event as ClipboardEvent;
+        clipboardEvent.preventDefault();
 
-    const data: any = JSON.parse(clipboardEvent.clipboardData.getData("text/json"));
-    if (data.model === undefined) {
-        return;
-    }
+        const data: any = JSON.parse(clipboardEvent.clipboardData.getData("text/json"));
+        if (data === undefined) {
+            return;
+        }
 
-    let graphic: IGraphic;
-    data.model.id = generateId();
-    if (data.type === "Rectangle") {
-        graphic = new Rectangle(data.model);
-    } else if (data.type === "Ellipse") {
-        graphic = new Ellipse(data.model);
-    } else if (data.type === "Curve") {
-        data.model.points = data.model.points.map((point: { x: number, y: number }) => new Point(point.x, point.y));
-        graphic = new Curve(data.model);
-    } else if (data.type === "Sketch") {
-        data.model.points = data.model.points.map((point: { x: number, y: number }) => new Point(point.x, point.y));
-        graphic = new Sketch(data.model);
-    } else if (data.type === "Text") {
-        graphic = new Text(data.model);
-    }
-
-    app.$store.commit("addGraphic", { slideId: app.$store.getters.activeSlide.id, graphic: graphic! });
-    app.$store.commit("focusGraphic", graphic!);
-    app.$store.commit("styleEditorObject", graphic!);
-};
+        const graphic: IGraphic = parseGraphic(data);
+        graphic.id = generateId();
+        app.$store.commit("addGraphic", { slideId: app.$store.getters.activeSlide.id, graphic: graphic });
+        app.$store.commit("focusGraphic", graphic);
+        app.$store.commit("styleEditorObject", graphic);
+    };
+}
 
 function parseGraphic(json: any): IGraphic {
     if (json.id !== undefined && json.origin !== undefined && json.width !== undefined &&
