0cff353cd9b3397c9ea9f673171f6eabbb8971f3
Jake Koperski <wijakoperski@gmail.com>
2019-01-20 11:38:47 -0600

    Added Utilties unit tests

--- test/Utilities.spec.js
+++ test/Utilities.spec.js
@@ -1,4 +1,10 @@
 import Utilities from "../src/utilities/general";
+import Rectangle from "../src/models/Rectangle";
+import Point from "../src/models/Point";
+import Ellipse from "../src/models/Ellipse";
+import Sketch from "../src/models/Sketch";
+import Curve from "../src/models/Curve";
+import Text from "../src/models/Text";
 
 describe("Utilities", () => {
     it("generates unique ids", () => {
@@ -10,5 +16,144 @@ describe("Utilities", () => {
 
         // Assert
         expect(firstId).not.toBe(secondId);
+    });
+
+    it("parses rectangles", () => {
+        // Arrange
+        const graphic = {
+            origin: { x: 0, y: 0 },
+            width: 50,
+            height: 100,
+            fillColor: "blue",
+            strokeColor: "pink",
+            strokeWidth: 5,
+            rotation: 45
+        }, expectedRectangle = new Rectangle({
+            origin: new Point(0, 0),
+            width: 50,
+            height: 100,
+            fillColor: "blue",
+            strokeColor: "pink",
+            strokeWidth: 5,
+            rotation: 45
+        });
+
+        // Act
+        const actualRectangle = Utilities.parseGraphic(graphic);
+        actualRectangle.id = expectedRectangle.id;
+        actualRectangle.boundingBoxId = expectedRectangle.boundingBoxId;
+
+        // Assert
+        expect(actualRectangle).toEqual(expectedRectangle);
+    });
+
+    it("parses ellipses", () => {
+        // Arrange
+        const graphic = {
+            center: { x: 0, y: 0 },
+            width: 50,
+            height: 100,
+            fillColor: "blue",
+            strokeColor: "pink",
+            strokeWidth: 5,
+            rotation: 45
+        }, expectedEllipse = new Ellipse({
+            center: new Point(0, 0),
+            width: 50,
+            height: 100,
+            fillColor: "blue",
+            strokeColor: "pink",
+            strokeWidth: 5,
+            rotation: 45
+        });
+
+        // Act
+        const actualEllipse = Utilities.parseGraphic(graphic);
+        actualEllipse.id = expectedEllipse.id;
+        actualEllipse.boundingBoxId = expectedEllipse.boundingBoxId;
+
+        // Assert
+        expect(actualEllipse).toEqual(expectedEllipse);
+    });
+
+    it("parses sketches", () => {
+        // Arrange
+        const graphic = {
+            points: [ { x: 0, y: 10 }, { x: 5, y: 15 }],
+            fillColor: "blue",
+            strokeColor: "pink",
+            strokeWidth: 5,
+            rotation: 45
+        }, expectedSketch = new Sketch({
+            points: [ new Point(0, 10), new Point(5, 15) ],
+            fillColor: "blue",
+            strokeColor: "pink",
+            strokeWidth: 5,
+            rotation: 45
+        });
+
+        // Act
+        const actualSketch = Utilities.parseGraphic(graphic);
+        actualSketch.id = expectedSketch.id;
+        actualSketch.boundingBoxId = expectedSketch.boundingBoxId;
+
+        // Assert
+        expect(actualSketch).toEqual(expectedSketch);
+    });
+
+    it("parses curves", () => {
+        // Arrange
+        const graphic = {
+            points: [ { x: 0, y: 10 }, { x: 5, y: 15 }],
+            fillColor: "blue",
+            strokeColor: "pink",
+            strokeWidth: 5,
+            rotation: 45,
+            degree: 2
+        }, expectedCurve = new Curve({
+            points: [ new Point(0, 10), new Point(5, 15) ],
+            fillColor: "blue",
+            strokeColor: "pink",
+            strokeWidth: 5,
+            rotation: 45,
+            degree: 2
+        });
+
+        // Act
+        const actualCurve = Utilities.parseGraphic(graphic);
+        actualCurve.id = expectedCurve.id;
+        actualCurve.boundingBoxId = expectedCurve.boundingBoxId;
+
+        // Assert
+        expect(actualCurve).toEqual(expectedCurve);
+    });
+
+    it("parses textboxes", () => {
+        // Arrange
+        const graphic = {
+            origin: { x: 0, y: 10 },
+            content: "Hello World!",
+            fontSize: "12px",
+            fontWeight: "regular",
+            fontFamily: "Times New Roman",
+            fillColor: "blue",
+            rotation: 45
+        }, expectedTextbox = new Text({
+            origin: new Point(0, 10),
+            content: "Hello World!",
+            fontSize: "12px",
+            fontWeight: "regular",
+            fontFamily: "Times New Roman",
+            fillColor: "blue",
+            rotation: 45
+        });
+
+        // Act
+        const actualTextbox = Utilities.parseGraphic(graphic);
+        actualTextbox.id = expectedTextbox.id;
+        actualTextbox.boundingBoxId = expectedTextbox.boundingBoxId;
+
+        // Assert
+        expect(actualTextbox).toEqual(expectedTextbox);
     });
 });
