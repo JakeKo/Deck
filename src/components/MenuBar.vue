0025b645faa00c7367200a19f268541a766edb61
Jake Koperski <wijakoperski@gmail.com>
2019-02-25 12:13:10 -0600

    Generalized file upload interactions for images and presentations

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,43 +1,12 @@
 <template>
-<div id="menu-bar">
-    <label for="presentation-upload">Upload: </label>
-    <input type="file" id="presentation-upload" name="presentation-upload" accept="application/json" @change="uploadPresentation">
-</div>
+<div id="menu-bar"></div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
-import Slide from "../models/Slide";
-import Utilities from "../utilities/general";
-import IGraphic from "../models/graphics/IGraphic";
 
 @Component
-export default class MenuBar extends Vue {
-    private uploadPresentation(event: Event): void {
-        // Fetch the uploaded file and abort if no file was selected
-        const presentationFile: File = (event.target as HTMLInputElement).files![0];
-        if (presentationFile === undefined) {
-            return;
-        }
-
-        // Create the file reader and set the event handler after reading is complete
-        const fileReader: FileReader = new FileReader();
-        fileReader.onloadend = (event: FileReaderProgressEvent): void => {
-            const json: any = JSON.parse(event.target!.result);
-
-            const slides: Array<Slide> = json.map((slide: any): Slide => new Slide({
-                id: slide.id,
-                graphics: slide.graphics.map((graphic: any): IGraphic => Utilities.parseGraphic(graphic))
-            }));
-
-            this.$store.dispatch("resetPresentation", slides);
-        };
-
-        // Asynchronously read the uploaded presentation as text
-        fileReader.readAsText(presentationFile);
-        (event.target as HTMLInputElement).value = "";
-    }
-}
+export default class MenuBar extends Vue { }
 </script>
 
 <style lang="scss" scoped>
--- src/components/Toolbox.vue
+++ src/components/Toolbox.vue
@@ -6,9 +6,10 @@
     <tool @click="$store.commit('tool', 'rectangle')" :toolName="'rectangle'" :icon="'fas fa-square'" :isActive="$store.getters.tool.name === 'rectangle'"></tool>
     <tool @click="$store.commit('tool', 'ellipse')" :toolName="'ellipse'" :icon="'fas fa-circle'" :isActive="$store.getters.tool.name === 'ellipse'"></tool>
     <tool @click="$store.commit('tool', 'textbox')" :toolName="'text'" :icon="'fas fa-font'" :isActive="$store.getters.tool.name === 'textbox'"></tool>
-    <tool @click="input.click()" :toolName="'image'" :icon="'fas fa-image'" :isActive="false"></tool>
-    <tool @click="addVideo" :toolName="'video'" :icon="'fas fa-video'" :isActive="false"></tool>
+    <tool @click="uploadImage" :toolName="'image'" :icon="'fas fa-image'" :isActive="false"></tool>
+    <tool @click="uploadVideo" :toolName="'video'" :icon="'fas fa-video'" :isActive="false"></tool>
     <tool @click="$store.dispatch('export')" :toolName="'export'" :icon="'fas fa-cloud-download-alt'" :isActive="false"></tool>
+    <tool @click="uploadPresentation" :toolName="'load'" :icon="'fas fa-file-upload'" :isActive="false"></tool>
     <tool @click="$store.dispatch('save')" :toolName="'save'" :icon="'fas fa-save'" :isActive="false"></tool>
     <a href="https://github.com/JakeKo/Deck/issues/new/choose" target="blank" style="text-decoration: none">
         <tool :toolName="'feedback'" :icon="'fas fa-info'" :isActive="false"></tool>
@@ -22,6 +23,9 @@ import Tool from "./Tool.vue";
 import Image from "../models/graphics/Image";
 import Point from "../models/Point";
 import Video from "../models/graphics/Video";
+import Slide from "../models/Slide";
+import IGraphic from "../models/graphics/IGraphic";
+import Utilities from "../utilities/general";
 
 @Component({
     components: {
@@ -30,42 +34,60 @@ import Video from "../models/graphics/Video";
 })
 export default class Toolbox extends Vue {
     private input!: HTMLInputElement;
+    private fileReader!: FileReader;
 
     private mounted(): void {
+        this.fileReader = new FileReader();
+
         this.input = document.createElement("input");
         this.input.type = "file";
         this.input.style.display = "none";
-
         this.input.addEventListener("change", (event: Event): void => {
             // Fetch the uploaded file and abort if no file was selected
-            const imageFile: File = (event.target as HTMLInputElement).files![0];
-            if (imageFile === undefined) {
+            const file: File = (event.target as HTMLInputElement).files![0];
+            if (file === undefined) {
                 return;
             }
 
-            // Create the file reader and set the event handler after reading is complete
-            const fileReader: FileReader = new FileReader();
-            fileReader.onloadend = (event: FileReaderProgressEvent): void => {
-                if (this.$store.getters.activeSlide === undefined) {
-                    return;
-                }
-
-                const imageUrl: string = event.target!.result;
-                this.$store.commit("addGraphic", { slideId: this.$store.getters.activeSlide.id, graphic: new Image({ source: imageUrl }) });
-            };
-
             // Asynchronously read the uploaded presentation as text
-            fileReader.readAsDataURL(imageFile);
+            this.fileReader.readAsBinaryString(file);
             (event.target as HTMLInputElement).value = "";
         });
     }
 
-    private addVideo(): void {
+    private uploadImage(): void {
+        this.fileReader.onloadend = (event: FileReaderProgressEvent): void => {
+            if (this.$store.getters.activeSlide === undefined) {
+                return;
+            }
+
+            const imageUrl: string = `data:image;base64,${btoa(this.fileReader.result)}`;
+            this.$store.commit("addGraphic", { slideId: this.$store.getters.activeSlide.id, graphic: new Image({ source: imageUrl }) });
+        };
+
+        this.input.click();
+    }
+
+    private uploadVideo(): void {
         const prompt: string | null = window.prompt("Enter a link to the video you would like add:");
 
         if (prompt !== null && prompt !== "") {
             this.$store.commit("addGraphic", { slideId: this.$store.getters.activeSlide.id, graphic: new Video({ source: prompt }) });
         }
+    }
+
+    private uploadPresentation(): void {
+        this.fileReader.onloadend = (event: FileReaderProgressEvent): void => {
+            const json: any = JSON.parse(this.fileReader.result);
+            const slides: Array<Slide> = json.map((slide: any): Slide => new Slide({
+                id: slide.id,
+                graphics: slide.graphics.map((graphic: any): IGraphic => Utilities.parseGraphic(graphic))
+            }));
+
+            this.$store.dispatch("resetPresentation", slides);
+        };
+
+        this.input.click();
     }
 }
 </script>
