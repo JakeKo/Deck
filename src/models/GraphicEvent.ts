e643f4fa514e13e71e5401cea3c5c79214619773
Jake Koperski <wijakoperski@gmail.com>
2019-03-05 15:53:53 -0600

    Cleaned up graphic events

---
+++ src/models/GraphicEvent.ts
@@ -0,0 +1,13 @@
+import IGraphic from "./graphics/IGraphic";
+
+export default class GraphicEvent {
+    public slideId: string;
+    public graphicId?: string;
+    public graphic?: IGraphic;
+
+    constructor(slideId: string, graphicId: string, graphic: IGraphic) {
+        this.slideId = slideId;
+        this.graphicId = graphicId;
+        this.graphic = graphic;
+    }
+}
