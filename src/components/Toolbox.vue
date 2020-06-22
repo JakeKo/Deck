2cb808c0f02a829392215d8b77eee56b1b118437
Jake Koperski <wijakoperski@gmail.com>
2020-06-22 02:13:53 -0500

    Added buttons for each tool

--- src/components/Toolbox.vue
+++ src/components/Toolbox.vue
@@ -2,14 +2,18 @@
 <div id='toolbox'>
     <tool :name='"pointer"' :icon='"fas fa-mouse-pointer"' :isActive='isPointerToolActive' @tool-click='activatePointerTool' />
     <tool :name='"rectangle"' :icon='"fas fa-square"' :isActive='isRectangleToolActive' @tool-click='activateRectangleTool' />
+    <tool :name='"ellipse"' :icon='"fas fa-circle"' :isActive='isEllipseToolActive' @tool-click='activateEllipseTool' />
     <tool :name='"curve"' :icon='"fas fa-pen-nib"' :isActive='isCurveToolActive' @tool-click='activateCurveTool' />
+    <tool :name='"textbox"' :icon='"fas fa-font"' :isActive='isTextboxToolActive' @tool-click='activateTextboxTool' />
+    <tool :name='"image"' :icon='"fas fa-image"' :isActive='isImageToolActive' @tool-click='activateImageTool' />
+    <tool :name='"video"' :icon='"fas fa-video"' :isActive='isVideoToolActive' @tool-click='activateVideoTool' />
 </div>
 </template>
 
 <script lang='ts'>
 import { Vue, Component, Prop } from 'vue-property-decorator';
 import Tool from './Tool.vue';
-import { PointerTool, RectangleTool, CurveTool } from '../tools';
+import { PointerTool, RectangleTool, CurveTool, EllipseTool, TextboxTool, ImageTool, VideoTool } from '../tools';
 import { MUTATIONS, GETTERS } from '../store/types';
 import { TOOL_NAMES, EditorTool } from '../tools/types';
 import { Getter, Mutation } from 'vuex-class';
@@ -31,8 +35,24 @@ export default class Toolbox extends Vue {
         return this[GETTERS.ACTIVE_TOOL_NAME] === TOOL_NAMES.RECTANGLE;
     }
 
+    private get isEllipseToolActive(): boolean {
+        return this[GETTERS.ACTIVE_TOOL_NAME] === TOOL_NAMES.ELLIPSE;
+    }
+
     private get isCurveToolActive(): boolean {
         return this[GETTERS.ACTIVE_TOOL_NAME] === TOOL_NAMES.CURVE;
+    }
+
+    private get isTextboxToolActive(): boolean {
+        return this[GETTERS.ACTIVE_TOOL_NAME] === TOOL_NAMES.TEXTBOX;
+    }
+
+    private get isImageToolActive(): boolean {
+        return this[GETTERS.ACTIVE_TOOL_NAME] === TOOL_NAMES.IMAGE;
+    }
+
+    private get isVideoToolActive(): boolean {
+        return this[GETTERS.ACTIVE_TOOL_NAME] === TOOL_NAMES.VIDEO;
     }
 
     private activatePointerTool(): void {
@@ -43,8 +63,24 @@ export default class Toolbox extends Vue {
         this[MUTATIONS.ACTIVE_TOOL](RectangleTool(this.$store));
     }
 
+    private activateEllipseTool(): void {
+        this[MUTATIONS.ACTIVE_TOOL](EllipseTool(this.$store));
+    }
+
     private activateCurveTool(): void {
         this[MUTATIONS.ACTIVE_TOOL](CurveTool(this.$store));
+    }
+
+    private activateTextboxTool(): void {
+        this[MUTATIONS.ACTIVE_TOOL](TextboxTool(this.$store));
+    }
+
+    private activateImageTool(): void {
+        this[MUTATIONS.ACTIVE_TOOL](ImageTool(this.$store));
+    }
+
+    private activateVideoTool(): void {
+        this[MUTATIONS.ACTIVE_TOOL](VideoTool(this.$store));
     }
 }
 </script>
