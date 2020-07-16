d7ded417883cca98b60ef87c044a107f9f555bac
Jake Koperski <wijakoperski@gmail.com>
2020-07-16 12:26:45 -0500

    Implemented theme for Roadmap component

--- src/components/Roadmap.vue
+++ src/components/Roadmap.vue
@@ -1,13 +1,13 @@
 <template>
-<div id='roadmap'>
+<div :style="roadmapStyle">
     <roadmap-slot v-for='slide in getRoadmapSlides'
         :key='slide.id'
         :id='slide.id'
         :isActive='slide.isActive'
     />
-    <div id='add-slide-slot' @click='createNewSlide'>
-        <div id='add-slide-label'>Add Slide</div>
-        <div id='add-slide-button'>
+    <div :style="addSlideSlotStyle" @click='createNewSlide'>
+        <div :style="addSlideLabelStyle">Add Slide</div>
+        <div :style="addSlideButtonStyle">
             <i class='fas fa-plus' />
         </div>
     </div>
@@ -19,13 +19,51 @@ import { Vue, Component } from 'vue-property-decorator';
 import RoadmapSlot from './RoadmapSlot.vue';
 import { MUTATIONS, GETTERS, RoadmapSlide, Slide } from '../store/types';
 import { Getter, Mutation } from 'vuex-class';
+import { StyleCreator } from '../styling/types';
+import DeckComponent from './generic/DeckComponent';
+
+type StyleProps = {};
+type Style = {
+    roadmap: any;
+    addSlideSlot: any;
+    addSlideLabel: any;
+    addSlideButton: any;
+};
+const componentStyle: StyleCreator<StyleProps, Style> = ({ theme, base, props }) => ({
+    roadmap: {
+        boxSizing: 'border-box',
+        borderTop: `1px solid ${theme.color.base.flush}`,
+        height: '80px',
+        ...base.flexRow,
+        flexShrink: '0',
+        overflowX: 'scroll'
+    },
+    addSlideSlot: {
+        height: '100%',
+        padding: '8px',
+        boxSizing: 'border-box',
+        ...base.flexColCC,
+        justifyContent: 'space-between',
+        cursor: 'pointer'
+    },
+    addSlideLabel: {
+        ...base.fontBody
+    },
+    addSlideButton: {
+        height: '45px',
+        width: '80px',
+        ...base.flexRowCC,
+        color: theme.color.base.highest,
+        background: theme.color.primary.flush
+    }
+});
 
 @Component({
     components: {
         RoadmapSlot
     }
 })
-export default class Roadmap extends Vue {
+export default class Roadmap extends DeckComponent<StyleProps, Style> {
     @Getter private [GETTERS.ROADMAP_SLIDES]: RoadmapSlide[];
     @Getter private [GETTERS.ACTIVE_SLIDE]: Slide;
     @Getter private [GETTERS.SLIDES]: Slide[];
@@ -37,47 +75,30 @@ export default class Roadmap extends Vue {
         this[MUTATIONS.ADD_SLIDE](this[GETTERS.SLIDES].length);
         this[MUTATIONS.ACTIVE_SLIDE_ID](this[GETTERS.LAST_SLIDE].id);
     }
+
+    private get roadmapStyle(): void {
+        const style = this[GETTERS.STYLE]({}, componentStyle);
+        return style.roadmap;
+    }
+
+    private get addSlideSlotStyle(): void {
+        const style = this[GETTERS.STYLE]({}, componentStyle);
+        return style.addSlideSlot;
+    }
+
+    private get addSlideLabelStyle(): void {
+        const style = this[GETTERS.STYLE]({}, componentStyle);
+        return style.addSlideLabel;
+    }
+
+    private get addSlideButtonStyle(): void {
+        const style = this[GETTERS.STYLE]({}, componentStyle);
+        return style.addSlideButton;
+    }
 }
 </script>
 
-<style lang='scss' scoped>
-@import '../styles/application';
-
-#roadmap {
-    box-sizing: border-box;
-    border-top: 1px solid $color-tertiary;
-    height: 80px;
-    display: flex;
-    flex-shrink: 0;
-    overflow-x: scroll;
-}
-
-#add-slide-slot {
-    height: 100%;
-    padding: 8px;
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    cursor: pointer;
-}
-
-#add-slide-label {
-    font-family: 'Roboto', sans-serif;
-    font-size: 14px;
-}
-
-#add-slide-button {
-    height: 45px;
-    width: 80px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: white;
-    background: $color-information;
-}
-
+<style lang="scss" scoped>
 ::-webkit-scrollbar {
     display: none;
 }
