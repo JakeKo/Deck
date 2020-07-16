91edb9245ab9e7e5ca9521b86d994b57434ae29f
Jake Koperski <wijakoperski@gmail.com>
2020-07-16 12:00:45 -0500

    Implemented theme for RoadmapSlot component

--- src/components/RoadmapSlot.vue
+++ src/components/RoadmapSlot.vue
@@ -1,50 +1,69 @@
 <template>
-    <div class='roadmap-slot' @click='() => setActiveSlideId(id)'>
-        <div class='slide-topic'>Topic</div>
-        <canvas :class='{ "slide-preview": true, "active-slide-preview": isActive }' />
+    <div :style='roadmapSlotStyle' @click='() => setActiveSlideId(id)'>
+        <div :style="slideTopicStyle">Topic</div>
+        <canvas :style="slidePreviewStyle" />
     </div>
 </template>
 
 <script lang='ts'>
 import { Vue, Component, Prop } from 'vue-property-decorator';
-import { MUTATIONS } from '../store/types';
+import { MUTATIONS, GETTERS } from '../store/types';
 import { Mutation } from 'vuex-class';
+import { StyleCreator } from '../styling/types';
+import DeckComponent from './generic/DeckComponent';
+
+type StyleProps = {};
+type Style = {
+    roadmapSlot: any;
+    slideTopic: any;
+    slidePreview: any;
+    activeSlidePreview: any;
+};
+const componentStyle: StyleCreator<StyleProps, Style> = ({ theme, base, props }) => ({
+    roadmapSlot: {
+        height: '100%',
+        padding: '8px',
+        boxSizing: 'border-box',
+        ...base.flexColCC,
+        justifyContent: 'space-between',
+        cursor: 'pointer'
+    },
+    slideTopic: {
+        ...base.fontBody
+    },
+    slidePreview: {
+        height: '45px',
+        width: '80px',
+        border: `2px solid ${theme.color.base.flush}`,
+        boxSizing: 'border-box'
+    },
+    activeSlidePreview: {
+        border: `2px solid ${theme.color.primary.flush}`
+    }
+});
 
 @Component
-export default class RoadmapSlot extends Vue {
+export default class RoadmapSlot extends DeckComponent<StyleProps, Style> {
     @Prop({ type: String, required: true }) private id!: string;
     @Prop({ type: Boolean, required: true }) private isActive!: boolean;
     @Mutation private [MUTATIONS.ACTIVE_SLIDE_ID]: (id: string) => void;
+
+    private get roadmapSlotStyle(): any {
+        const style = this[GETTERS.STYLE]({}, componentStyle);
+        return style.roadmapSlot;
+    }
+
+    private get slideTopicStyle(): any {
+        const style = this[GETTERS.STYLE]({}, componentStyle);
+        return style.slideTopic;
+    }
+
+    private get slidePreviewStyle(): any {
+        const style = this[GETTERS.STYLE]({}, componentStyle);
+        return {
+            ...style.slidePreview,
+            ...this.isActive && style.activeSlidePreview
+        };
+    }
 }
 </script>
-
-<style lang='scss' scoped>
-@import '../styles/colors';
-
-.roadmap-slot {
-    height: 100%;
-    padding: 8px;
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    cursor: pointer;
-}
-
-.slide-topic {
-    font-family: 'Roboto', sans-serif;
-    font-size: 14px;
-}
-
-.slide-preview {
-    height: 45px;
-    width: 80px;
-    border: 2px solid $color-tertiary;
-    box-sizing: border-box;
-}
-
-.active-slide-preview {
-    border: 2px solid $color-information;
-}
-</style>
