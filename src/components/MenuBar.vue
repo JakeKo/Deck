96e44f6bce50eddc0d9f34ba846f38db42782bbb
Jake Koperski <wijakoperski@gmail.com>
2020-07-15 23:25:23 -0500

    Implemented theme for MenuBar

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,16 +1,51 @@
 <template>
-<div id='menu-bar'>
-    <input id='deck-title' v-model='deckTitle' @keydown='handleKeydown' autocomplete='off' placeholder='Untitled'>
+<div :style="menuBarStyle">
+    <input :style="deckTitleStyle" v-model='deckTitle' @keydown='handleKeydown' autocomplete='off' placeholder='Untitled'>
 </div>
 </template>
 
 <script lang='ts'>
-import { Vue, Component } from 'vue-property-decorator';
+import { Component } from 'vue-property-decorator';
 import { MUTATIONS, GETTERS } from '../store/types';
 import { Getter, Mutation } from 'vuex-class';
+import { StyleCreator } from '../styling/types';
+import DeckComponent from './DeckComponent';
+
+type StyleProps = {};
+type Style = {
+    menuBar: any;
+    deckTitle: any;
+    deckTitleHover: any;
+    deckTitleFocus: any;
+};
+const menuBarStyle: StyleCreator<StyleProps, Style> = ({ theme, base, props }) => ({
+    menuBar: {
+        height: '28px',
+        flexShrink: '0',
+        boxSizing: 'border-box',
+        borderBottom: `1px solid ${theme.color.base.flush}`,
+        background: theme.color.base.highest,
+        ...base.fontBody,
+        ...base.flexRowCC
+    },
+    deckTitle: {
+        outline: 'none',
+        border: 'none',
+        textAlign: 'center',
+        ...base.fontBody,
+        transition: '0.25s',
+        background: 'transparent'
+    },
+    deckTitleHover: {
+        background: theme.color.base.higher
+    },
+    deckTitleFocus: {
+        background: theme.color.base.flush
+    }
+});
 
 @Component
-export default class MenuBar extends Vue {
+export default class MenuBar extends DeckComponent<StyleProps, Style> {
     @Getter private [GETTERS.DECK_TITLE]: string;
     @Mutation private [MUTATIONS.DECK_TITLE]: (deckTitle: string) => void;
 
@@ -22,6 +57,16 @@ export default class MenuBar extends Vue {
         this[MUTATIONS.DECK_TITLE](deckTitle);
     }
 
+    private get menuBarStyle(): any {
+        const style = this[GETTERS.STYLE]({}, menuBarStyle);
+        return style.menuBar;
+    }
+
+    private get deckTitleStyle(): any {
+        const style = this[GETTERS.STYLE]({}, menuBarStyle);
+        return style.deckTitle;
+    }
+
     private handleKeydown(event: KeyboardEvent): void {
         if (['Tab', 'Enter', 'Escape'].indexOf(event.key) !== -1) {
             (event.target as HTMLInputElement).blur();
@@ -29,38 +74,3 @@ export default class MenuBar extends Vue {
     }
 }
 </script>
-
-<style lang='scss' scoped>
-@import '../styles/application';
-
-#menu-bar {
-    height: 28px;
-    flex-shrink: 0;
-    box-sizing: border-box;
-    border-bottom: 1px solid $color-tertiary;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-family: $font-body;
-}
-
-#deck-title {
-    outline: none;
-    border: none;
-    text-align: center;
-    font-family: $font-body;
-    font-size: 14px;
-    transition: 0.25s;
-    height: 100%;
-    background: transparent;
-
-    &:hover {
-        background: $color-secondary;
-    }
-
-    &:focus {
-        background: $color-tertiary;
-    }
-}
-</style>
-
