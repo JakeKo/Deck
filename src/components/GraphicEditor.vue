cf44fd4fe37fe8f24c3e98c4533ec9ee9054967d
Jake Koperski <wijakoperski@gmail.com>
2020-09-11 09:18:41 -0500

    Created initial RectangleEditorForm

---
+++ src/components/GraphicEditor.vue
@@ -0,0 +1,34 @@
+<template>
+    <div ref='root' :style='style.graphicEditor'>
+        <RectangleEditorForm />
+    </div>
+</template>
+
+<script lang='ts'>
+import { computed, defineComponent, reactive } from 'vue';
+import DeckComponent from './generic/DeckComponent';
+import RectangleEditorForm from './graphicEditorForms/RectangleEditorForm.vue';
+
+const GraphicEditor = defineComponent({
+    components: {
+        RectangleEditorForm
+    },
+    setup: () => {
+        const { root, store, baseTheme } = DeckComponent();
+        const style = reactive({
+            graphicEditor: computed(() => ({
+                height: '100%',
+                width: '320px',
+                flexShrink: '0'
+            }))
+        });
+
+        return {
+            root,
+            style
+        };
+    }
+});
+
+export default GraphicEditor;
+</script>
---
+++ src/components/graphicEditorForms/RectangleEditorForm.vue
@@ -0,0 +1,44 @@
+<template>
+    <div ref='root' :style='style.rectangleEditorForm'>
+        <label for='x'>X</label>
+        <input name='x' type='number' /><br />
+        <label for='y'>Y</label>
+        <input name='y' type='number' /><br />
+        <label for='width'>Width</label>
+        <input name='width' type='number' /><br />
+        <label for='height'>Height</label>
+        <input name='height' type='number' /><br />
+        <label for='rotation'>Rotation</label>
+        <input name='rotation' type='number' /><br />
+        <label for='stroke-width'>Stroke Width</label>
+        <input name='stroke-width' type='number' /><br />
+        <label for='fill-color'>Fill Color</label>
+        <input name='fill-color' type='text' /><br />
+        <label for='stroke-color'>Stroke Color</label>
+        <input name='stroke-color' type='text' /><br />
+    </div>
+</template>
+
+<script lang='ts'>
+import { computed, defineComponent, reactive } from 'vue';
+import DeckComponent from '../generic/DeckComponent';
+
+const RectangleEditorForm = defineComponent({
+    setup: () => {
+        const { root, store, baseTheme } = DeckComponent();
+        const style = reactive({
+            rectangleEditorForm: computed(() => ({
+                height: '100%',
+                width: '100%'
+            }))
+        });
+
+        return {
+            root,
+            style
+        };
+    }
+});
+
+export default RectangleEditorForm;
+</script>
