02bb76118f8a75691e148b8a1754c99a19a54890
Jake Koperski <wijakoperski@gmail.com>
2020-10-10 07:31:20 -0500

    Fixed broken IdProvider test

--- src/utilities/__tests__/IdProvider.spec.ts
+++ src/utilities/__tests__/IdProvider.spec.ts
@@ -1,11 +1,12 @@
 import * as IdProvider from '../IdProvider';
 
 describe('IdProvider', () => {
-    it('generates unique ids', () => {
+    it('generates unique ids', async () => {
         // Arrange
 
         // Act
         const id1 = IdProvider.provideId();
+        await new Promise(resolve => setTimeout(resolve, 10));
         const id2 = IdProvider.provideId();
 
         // Assert
